545ca09ca78222a2dcf790981e98a4a7a89a4e5f
chine <charleyh@graphicly.com>
2010-11-21 18:49:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsbebuds::Application.config.secret_token = 'fd69c1906215cbdcee172e924f520436fdb4a29f1383cf901a84816eb13af54bd5401065468b98ba0d4d812b21ed97214124924867d83431127722dfb7f38702'
