b5b50eeea6845c53a5af5437fa4dbe02daae491d
Charley Hine <charles.hine@gmail.com>
2010-11-26 12:06:57 -0700

    added registration route

--- deal_site/config/routes.rb
+++ deal_site/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
     match '/checkout' => 'checkout#edit', :state => 'payment', :as => :checkout
-    #match '/login' => 'checkout#edit', :state => 'payment', :as => :login
+    match '/login' => 'checkout#edit', :state => 'registration', :as => :login
 end
