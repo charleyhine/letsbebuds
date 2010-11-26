cabeed2224f89969c21ffb7d8571b02a21260b08
Charley Hine <charles.hine@gmail.com>
2010-11-26 02:18:01 -0700

    ssl tinkeringers

--- deal_site/lib/spree_core/ssl_requirement.rb
+++ deal_site/lib/spree_core/ssl_requirement.rb
@@ -92,7 +92,7 @@ module SslRequirement
     def ensure_proper_protocol
       return true if ssl_allowed?
       if ssl_required? && !request.ssl? && ssl_supported?
-        redirect_to "https://secure." + request.host + request.fullpath
+        redirect_to "https://secure.letsbebuds.com" + request.fullpath
         flash.keep
       elsif request.ssl? && !ssl_required?
         redirect_to "http://" + request.host + request.fullpath
