d99050adcf690f2223b4b8f21601045759f7d14e
Charley Hine <charles.hine@gmail.com>
2010-12-01 11:55:48 -0700

    session across mulitple domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Letsbebuds::Application.config.session_store :cookie_store, :key => '_letsbebuds_session'
+
+Letsbebuds::Application.config.session_store :cookie_store, :key => '_letsbebuds_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
