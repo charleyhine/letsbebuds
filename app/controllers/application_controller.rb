0d77462e55bd10ef12e7db5caf7cbb328787cfc5
Charley Hine <charles.hine@gmail.com>
2010-12-01 11:58:29 -0700

    removed www redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_filter :check_uri
+  #before_filter :check_uri
   protect_from_forgery
 
-  def check_uri
-    if /^www/.match(request.host)
-      redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
-    end
-  end
+  #def check_uri
+  #  if /^www/.match(request.host)
+  #   redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
+  #  end
+  #end
 end
