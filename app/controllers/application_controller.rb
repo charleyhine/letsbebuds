78cf807fa4b12e7914b08675e77d1a535415f3b2
Charley Hine <charles.hine@gmail.com>
2010-12-01 11:40:35 -0700

    no www allowed!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_uri
+
+  def check_uri
+    if /^www/.match(request.host)
+      redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
+    end
+  end
 end
