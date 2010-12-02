2b87097d64ae0f684ca89f69deceac7b611f3c20
Charley Hine <charles.hine@gmail.com>
2010-12-02 10:39:52 -0700

    check mic 123

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -1,11 +1,11 @@
 module SpreeSite
   class Engine < Rails::Engine
     def self.activate
+        state_machines.clear 
         Order.class_eval do 
               state_machines[:state] = StateMachine::Machine.new(Order, 
       :initial => 'cart', :use_transactions => false) do  # this is the line that changed 
                 event :next do 
-                  state_machines.clear 
                   transition :from => 'cart', :to => 'payment'
                   #transition :from => 'address', :to => 'payment'
                   #transition :from => 'delivery', :to => 'payment'
