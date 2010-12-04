5fda83788ce9dc3ec2df99667c70f67d9054cf4d
Charley Hine <charles.hine@gmail.com>
2010-12-04 11:51:58 -0700

    Removed deal_site folder

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-
-Letsbebuds::Application.config.session_store :cookie_store, :key => '_letsbebuds_session', :domain => "letsbebuds.com"
+if RAILS_ENV = 'production'
+  Letsbebuds::Application.config.session_store :cookie_store, :key => '_letsbebuds_session', :domain => "letsbebuds.com"
+else
+  Letsbebuds::Application.config.session_store :cookie_store, :key => '_letsbebuds_session' 
+end  
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
